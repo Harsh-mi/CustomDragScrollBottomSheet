3ee7896afe172371106dd2f2ebe70315368350f0
Harsh Parikh <harsh.parikh@mindinventory.com>
2024-05-21 15:12:37 +0530

    Testing commit

--- Sources/CustomDragScrollBottomSheet/CustomDragScrollBottomSheet.swift
+++ Sources/CustomDragScrollBottomSheet/CustomDragScrollBottomSheet.swift
@@ -62,7 +62,7 @@ public class CustomDragScrollBottomSheetVC: UIViewController {
     func configBottomSheet() {
         
         bottomSheetPanGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
-        bottomSheetView.frame = CGRect(x: 0, y: view.frame.midY - 50, width: view.frame.width, height: fullHeight)
+        bottomSheetView.frame = CGRect(x: 0, y: view.frame.midY - 70, width: view.frame.width, height: fullHeight)
         bottomSheetView.backgroundColor = .white
         bottomSheetView.layer.cornerRadius = 10
         bottomSheetView.layer.shadowColor = UIColor.gray.cgColor
@@ -90,7 +90,7 @@ public class CustomDragScrollBottomSheetVC: UIViewController {
         layout.scrollDirection = .vertical
         
         collectionViewPostOptions = UICollectionView(frame: CGRect(x: 0, y: 30, width: view.frame.width, height: halfHeight), collectionViewLayout: layout)
-        collectionViewPostOptions.contentInset.bottom = isHorizontalLayout ? 0 : (isFullHeight ? 0 : 20)
+        collectionViewPostOptions.contentInset.bottom = isHorizontalLayout ? 0 : (isFullHeight ? 0 : 40)
         collectionViewPostOptions.translatesAutoresizingMaskIntoConstraints = false
         collectionViewPostOptions.showsHorizontalScrollIndicator = false
         collectionViewPostOptions.showsVerticalScrollIndicator = false
